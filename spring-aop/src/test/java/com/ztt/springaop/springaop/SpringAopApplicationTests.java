500820a31e8507f0e6d88370902f52ac39aecd2c
hzmeric <huangzm@imaidao.cn>
2018-08-28 23:06:08 +0800

    提交

--- spring-aop/src/test/java/com/ztt/springaop/springaop/SpringAopApplicationTests.java
+++ spring-aop/src/test/java/com/ztt/springaop/springaop/SpringAopApplicationTests.java
@@ -1,12 +1,15 @@
 package com.ztt.springaop.springaop;
 
-import com.ztt.springaop.springaop.dto.EmployeeDTO;
+import com.google.common.collect.Lists;
 import com.ztt.springaop.springaop.service.EmployeeManager;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -16,9 +19,21 @@ public class SpringAopApplicationTests {
     private EmployeeManager manager;
     @Test
     public void contextLoads() {
-        manager.getEmployeeById(1);
-        manager.createEmployee(new EmployeeDTO());
-        manager.deleteEmployee(100);
+//        manager.getEmployeeById(1);
+//        manager.createEmployee(new EmployeeDTO());
+//        manager.deleteEmployee(100);
+        List<String> list = Lists.newArrayList();
+        list.add("a");
+        list.add("b");
+        List<String> stringList = list;
+        list = stringList.stream().filter(t->t.equals("b")).collect(Collectors.toList());
+        stringList.forEach(System.out::println);
+        list = list.stream().filter(t->t.equals("a")).collect(Collectors.toList());
+
+
+        list.forEach(System.out::println);
+
+
     }
 
 }
--- spring-test/src/test/java/com/tk/springtest/SpringTestApplicationTests.java
+++ spring-test/src/test/java/com/tk/springtest/SpringTestApplicationTests.java
@@ -1,69 +1,69 @@
-package com.tk.springtest;
-
-import com.alibaba.fastjson.JSON;
-import com.tk.springtest.web.po.Department;
-import com.tk.springtest.web.po.DeptEmp;
-import com.tk.springtest.web.po.Employee;
-
-import org.assertj.core.util.Lists;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import java.math.BigDecimal;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class SpringTestApplicationTests {
+//package com.tk.springtest;
 //
-//    @Test
-//    public void contextLoads() {
-//    }
-public static void main(String[] args) {
-    // product
-//    Employee employee = Employee.builder()
-//            .firstName("1")
-//            .empNo(1)
-//            .build();
-//    System.out.println("employee: " +JSON.toJSON(employee));
+//import com.alibaba.fastjson.JSON;
+//import com.tk.springtest.web.po.Department;
+//import com.tk.springtest.web.po.DeptEmp;
+//import com.tk.springtest.web.po.Employee;
 //
-//    // foot
-//    Department department = Department.builder()
-//            .a("7")
-//            .build();
-//    System.out.println("department: " +JSON.toJSON(department));
+//import org.assertj.core.util.Lists;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
 //
-//    // view
-//    DeptEmp deptEmp = DeptEmp.builder()
-//            .b(employee.getFirstName())
-//            .a(department.getA())
-//            .build();
-//    System.out.println("deptEmp: " +JSON.toJSON(deptEmp));
+//import java.math.BigDecimal;
+//import java.util.Arrays;
+//import java.util.HashMap;
+//import java.util.List;
+//import java.util.Map;
+//import java.util.stream.Collectors;
 //
-//    Map<Integer, Integer> j = new HashMap<>();
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class SpringTestApplicationTests {
+////
+////    @Test
+////    public void contextLoads() {
+////    }
+//public static void main(String[] args) {
+//    // product
+////    Employee employee = Employee.builder()
+////            .firstName("1")
+////            .empNo(1)
+////            .build();
+////    System.out.println("employee: " +JSON.toJSON(employee));
+////
+////    // foot
+////    Department department = Department.builder()
+////            .a("7")
+////            .build();
+////    System.out.println("department: " +JSON.toJSON(department));
+////
+////    // view
+////    DeptEmp deptEmp = DeptEmp.builder()
+////            .b(employee.getFirstName())
+////            .a(department.getA())
+////            .build();
+////    System.out.println("deptEmp: " +JSON.toJSON(deptEmp));
+////
+////    Map<Integer, Integer> j = new HashMap<>();
+////
+////    j.put(1,1);
+////    System.out.println(j.get(2));
+////    BigDecimal b1 =new BigDecimal(100);
+////    BigDecimal b2  = new BigDecimal(10);
+////    int groupCount = (int)Math
+////            .floor(Double.parseDouble(
+////                    String.valueOf(b1.multiply(b2))))/101;
+////    System.out.println(groupCount);
+////    double a = Math.floor(groupCount);
+//    List<String> list = Lists.newArrayList();
+//    list.add("A");
+//    list.add("B");
+////    List<String> listB = Lists.newArrayList();
+////    listB.add("A");
+////    list=list.stream().filter(t-> !listB.contains(t)).collect(Collectors.toList());
+//    System.out.println(Arrays.toString(list.toArray()));
 //
-//    j.put(1,1);
-//    System.out.println(j.get(2));
-//    BigDecimal b1 =new BigDecimal(100);
-//    BigDecimal b2  = new BigDecimal(10);
-//    int groupCount = (int)Math
-//            .floor(Double.parseDouble(
-//                    String.valueOf(b1.multiply(b2))))/101;
-//    System.out.println(groupCount);
-//    double a = Math.floor(groupCount);
-    List<String> list = Lists.newArrayList();
-    list.add("A");
-    list.add("B");
-//    List<String> listB = Lists.newArrayList();
-//    listB.add("A");
-//    list=list.stream().filter(t-> !listB.contains(t)).collect(Collectors.toList());
-    System.out.println(Arrays.toString(list.toArray()));
-
-}
-}
+//}
+//}
